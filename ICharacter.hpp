c40d9c11be79756e95a6577f2c02d7c078a51de2
Ahmed Alshafy <aalshafy@student.42abudhabi.ae>
2024-09-01 10:58:39 +0400

    small edit

--- ICharacter.hpp
+++ ICharacter.hpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "AMateria.hpp"
 
-#ifndef
-#define
+#ifndef ICharacter_HPP
+#define ICharacter_HPP
 
+class ICharacter{
+    public:
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
